a2edf6a48c8e8c5fcbeb6acddb607cbae5d38054
Philipdeve <thedeveloperphilip@gmail.com>
2024-11-11 13:01:11 +0100

    first commmit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+// src/data.js
+export const products = [
+    {
+      id: 1,
+      name: "Wireless Headphones",
+      price: 120,
+      image: "https://images.pexels.com/photos/3739116/pexels-photo-3739116.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 2,
+      name: "Smartphone",
+      price: 799,
+      image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTcwMjR8MHwxfGFsbHwxfHx8fHx8fHwxNjgzNTk5OTg3&ixlib=rb-1.2.1&q=80&w=600",
+    },
+    {
+      id: 3,
+      name: "Laptop",
+      price: 1500,
+      image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTcwMjR8MHwxfGFsbHwxfHx8fHx8fHwxNjgzNjAwMDEy&ixlib=rb-1.2.1&q=80&w=600",
+    },
+    {
+      id: 4,
+      name: "Smartwatch",
+      price: 199,
+      image: "https://images.pexels.com/photos/1908195/pexels-photo-1908195.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 5,
+      name: "Gaming Console",
+      price: 399,
+      image: "https://images.unsplash.com/photo-1593642532973-d31b6557fa68?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTcwMjR8MHwxfGFsbHwxfHx8fHx8fHwxNjgzNjAwMTUz&ixlib=rb-1.2.1&q=80&w=600",
+    },
+    {
+      id: 6,
+      name: "Bluetooth Speaker",
+      price: 99,
+      image: "https://images.pexels.com/photos/1309408/pexels-photo-1309408.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 7,
+      name: "Digital Camera",
+      price: 599,
+      image: "https://images.unsplash.com/photo-1539610318989-88406c7ba486?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTcwMjR8MHwxfGFsbHwxfHx8fHx8fHwxNjgzNjAwMDM1&ixlib=rb-1.2.1&q=80&w=600",
+    },
+    {
+      id: 8,
+      name: "Tablet",
+      price: 349,
+      image: "https://images.pexels.com/photos/5077400/pexels-photo-5077400.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 9,
+      name: "Fitness Tracker",
+      price: 49,
+      image: "https://images.unsplash.com/photo-1541753866388-0b3c701627d3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTcwMjR8MHwxfGFsbHwxfHx8fHx8fHwxNjgzNjAwMTg4&ixlib=rb-1.2.1&q=80&w=600",
+    },
+    {
+      id: 10,
+      name: "VR Headset",
+      price: 299,
+      image: "https://images.pexels.com/photos/4195325/pexels-photo-4195325.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ];
+  
